b16700aa16dbf57c1519a1074c37594c6b8d5293
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-05 00:58:23 +0100

    Create very naive prime nunmber generator

--- 003/script.js
+++ 003/script.js
@@ -1,2 +1,24 @@
 "use strict";
 console.log("script.js: flotation device");
+
+function isFactor(factor, number) {
+    return !(number % factor);
+}
+
+function* primeGen(max) {
+    let primes = [];
+    let current = 2;
+
+    while (current < max) {
+        let isPrime = true;
+        for (let item of primes) {
+            if (isFactor(item, current)) isPrime = false;
+        }
+        if (isPrime) yield current;
+        primes.push(current);
+        current++;
+    }
+
+}
+
+let generator = primeGen(40);
