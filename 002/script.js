93f6d2b8bf9f7cbff49693ef85948e936fd2647f
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-05 00:18:36 +0100

    Add modulo condition to generator

--- 002/script.js
+++ 002/script.js
@@ -1,18 +1,24 @@
 "use strict";
 console.log("script.js: flotation device");
 
-function* fibonacciGen(max) {
+function* fibonacciGen(max, div=1) {
+    if (div <= 0) throw new Error("Second parameter must be greater than 0"); 
+
     let current = 1;
     let next = 2;
+    
     while (current < max) {
-        yield current;
+        if (!(current % div)) yield current;
         [current, next] = [next, next + current];
     }
 }
 
-let iter = fibonacciGen(20);
+let generator = fibonacciGen(4000000, 2);
 let numArray = [];
 
-for (let item of iter) {
+// alternative method to "div=1" parameter would be to check (item % 2) here
+for (let item of generator) {
     numArray.push(item);
 }
+
+console.log(numArray);
