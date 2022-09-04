e5732bfe65fc076f54204756612e0eb1a6c52cdd
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-04 21:24:47 +0100

    Reorder range() function for legibility

--- 001/script.js
+++ 001/script.js
@@ -2,25 +2,26 @@
 console.log("script.js: flotation device");
 
 function range(start, end, step=1) {
-    let isReverse = false;
     for (let item of arguments) {
         if (item < 0) throw new Error("Negative number in range function");
     }
-    let arr = [];
+
+    let isReverse = false;
     if (end == null) {
-        console.log("hey");
         end = start;
         start = 0;
     } else if (end < start) {
         [start, end] = [end, start]; // ES6 destructuring instead of creating temp value
-
         isReverse = true;
     }
+
+    let arr = [];
     while (start < end) {
         arr.push(start);
         start += step;
     }
+    
     return isReverse ? arr.reverse() : arr;
 }
 
-console.log(range(5, 0));
+console.log(range(1000));
