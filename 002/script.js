9dde27f713157fe2153680db14c5fd26b3663e2c
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-05 00:20:03 +0100

    Copy over sumAll function

--- 002/script.js
+++ 002/script.js
@@ -1,6 +1,12 @@
 "use strict";
 console.log("script.js: flotation device");
 
+function sumAll(arr) {
+    return arr.reduce((acc, val) => {
+        return acc + val;
+    }, 0);
+}
+
 function* fibonacciGen(max, div=1) {
     if (div <= 0) throw new Error("Second parameter must be greater than 0"); 
 
@@ -21,4 +27,4 @@ for (let item of generator) {
     numArray.push(item);
 }
 
-console.log(numArray);
+console.log(sumAll(numArray));
