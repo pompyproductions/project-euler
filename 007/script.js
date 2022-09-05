dcd995d1930db26671aa1002a3eaf24455876685
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-05 20:31:09 +0100

    Copy over prime generator from 005

---
+++ 007/script.js
@@ -0,0 +1,30 @@
+"use strict";
+console.log("script.js: flotation device");
+
+function isFactor(factor, number) {
+    return !(number % factor);
+}
+
+function* primeGen(max=Infinity) {
+    yield 2;
+    let primes = [2];
+    let current = 3;
+
+    while (current < max) {
+        let isPrime = true;
+        for (let item of primes) {
+            if (isFactor(item, current)) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) yield current;
+        primes.push(current);
+        current += 2;
+    }
+}
+
+let generator = primeGen();
+console.log(generator.next().value);
+console.log(generator.next().value);
+console.log(generator.next().value);
