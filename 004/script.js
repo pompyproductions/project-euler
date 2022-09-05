2075cc85b5da6352b128e2a9eb6f3afdc123c7d9
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-05 10:09:27 +0100

    Initialize isPalindrome function

--- 004/script.js
+++ 004/script.js
@@ -1,2 +1,19 @@
 "use strict";
 console.log("script.js: flotation device");
+
+function isPalindrome(num) {
+    // accepts string and number, negatives too
+    if (isNaN(parseInt(num))) throw new Error("Not a number");
+    if (!num) return true;
+    if (num % 1) return false;
+
+    num = Math.abs(num).toString();
+    
+    return num;
+}
+
+
+function findPalindromeProduct(num, ...args) {
+    // takes one number and the following args, in order:
+    // from, to, step
+}
