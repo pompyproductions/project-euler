7eecec55226b1d001031decca16febdc9204780e
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-05 20:18:18 +0100

    Copy over range function from 005

--- 006/script.js
+++ 006/script.js
@@ -1,2 +1,25 @@
 "use strict";
 console.log("script.js: flotation device");
+
+function range(start, end, step=1) {
+    for (let item of arguments) {
+        if (item < 0) throw new Error("Negative number in range function");
+    }
+
+    let isReverse = false;
+    if (end == null) {
+        end = start;
+        start = 0;
+    } else if (end < start) {
+        [start, end] = [end, start]; // ES6 destructuring instead of creating temp value
+        isReverse = true;
+    }
+
+    let arr = [];
+    while (start < end) {
+        arr.push(start);
+        start += step;
+    }
+
+    return isReverse ? arr.reverse() : arr;
+}
