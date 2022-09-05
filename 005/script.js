ca389844a9fff6b22835b0843b44dfd496c5aceb
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-05 15:44:16 +0100

    Copy over factorization functions from 003

--- 005/script.js
+++ 005/script.js
@@ -1,2 +1,36 @@
 "use strict";
 console.log("script.js: flotation device");
+
+function isFactor(factor, number) {
+    return !(number % factor);
+}
+
+function* primeGen(max=Infinity) {
+    yield 2;
+    let primes = [2];
+    let current = 3;
+
+    while (current < max) {
+        let isPrime = true;
+        for (let item of primes) {
+            if (isFactor(item, current)) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) yield current;
+        primes.push(current);
+        current += 2;
+    }
+}
+
+function findPrimeFactor(num) {
+    let generator = primeGen();
+    let iter = generator.next();
+    while (num % iter.value) {
+        iter = generator.next();
+    };
+    return iter.value
+}
+
+console.log(findPrimeFactor(49));
