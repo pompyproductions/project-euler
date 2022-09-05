95faa1860e468c525814c10495d7046bc103b5fa
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-05 01:18:03 +0100

    Slightly less naive prime number generator

--- 003/script.js
+++ 003/script.js
@@ -5,28 +5,22 @@ function isFactor(factor, number) {
     return !(number % factor);
 }
 
-function* primeGen(max) {
-    let primes = [];
-    let current = 2;
+function* primeGen(max=5000) {
+    let primes = [2];
+    let current = 3;
 
     while (current < max) {
         let isPrime = true;
         for (let item of primes) {
-            if (isFactor(item, current)) isPrime = false;
+            if (isFactor(item, current)) {
+                isPrime = false;
+                break;
+            }
         }
         if (isPrime) yield current;
         primes.push(current);
-        current++;
+        current += 2;
     }
 }
 
-let generator = primeGen(40);
-let iter = generator.next();
-let largestPrimeFactor = 1;
-
-do {
-    largestPrimeFactor = iter.value;
-    iter = generator.next();
-} while (!iter.done);
-
-console.log(largestPrimeFactor);
+let generator = primeGen();
