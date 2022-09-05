7e667c602972e879dd2bee75bf99882b5825883f
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-05 15:49:08 +0100

    Copy over range function from 001

--- 005/script.js
+++ 005/script.js
@@ -43,4 +43,27 @@ function factorize(num) {
     return factors;
 }
 
-console.log(factorize(2452));
+function range(start, end, step=1) {
+    for (let item of arguments) {
+        if (item < 0) throw new Error("Negative number in range function");
+    }
+
+    let isReverse = false;
+    if (end == null) {
+        end = start;
+        start = 0;
+    } else if (end < start) {
+        [start, end] = [end, start]; // ES6 destructuring instead of creating temp value
+        isReverse = true;
+    }
+
+    let arr = [];
+    while (start < end) {
+        arr.push(start);
+        start += step;
+    }
+
+    return isReverse ? arr.reverse() : arr;
+}
+
+console.log(range(20));
