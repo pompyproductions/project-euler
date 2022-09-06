f103c0a5ae30c04461e4949e3afe8748a4704104
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-06 14:31:20 +0100

    Create untidy sequenceGen function

--- 008/script.js
+++ 008/script.js
@@ -1,4 +1,23 @@
 "use strict";
 console.log("script.js: flotation device");
 
-console.log(PROBLEM_INPUT);
+
+function* sequenceGen(input, length, callback, start=0) {
+    if (!callback) {
+        callback = function() {
+            console.log("Callback!");
+        }
+    }
+    for (let i = start; i < input.length - (length - 1); i++) {
+        if (!(input.substr(i, length).includes("0"))) {
+            yield input.substr(i, length);
+        } else {
+            console.log("found 0, skip");
+            i += length - 1;
+        }
+    }
+    
+}
+
+let generator = sequenceGen(PROBLEM_INPUT, 4);
+generator.next();
