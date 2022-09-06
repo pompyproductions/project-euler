ab50adc8e5a5df6cfdefdaf548dbac307e7e2659
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-07 00:40:30 +0100

    Get exactly 1 triple per value "a"
    Only after testing did it occur to me that there could
    be more than 1 pythagoran triple with the same value a
    for which a<b<c and a*a+b*b=c*c.
    
    Gonna have to refactor it all.

--- 009/script.js
+++ 009/script.js
@@ -10,8 +10,32 @@ function getPythagorean(a) {
         if (c === b + 1) return false;
         b++;
     }
-
     return [a,b,c]
 }
 
-console.log(getPythagorean(1));
+
+let currentTriple;
+// first find a pythagorean triple
+for (let a = 1; !currentTriple; a++) {
+    currentTriple = getPythagorean(a);
+}
+// then find more pythagoreans until the sum is 1000
+let a = currentTriple[0];
+
+while (currentTriple.reduce((acc, val)=>acc+val, 0) !== 1000) {
+    a++;
+    {
+        let nextTriple = getPythagorean(a);
+        if (nextTriple) {
+            currentTriple = nextTriple;
+        }
+        console.log(currentTriple.reduce((acc, val)=>acc+val, 0));
+    }
+    if (a > 500) break;
+}
+
+
+
+// for (a; currentTriple.reduce((acc, val)=>acc*val, 1) !== 1000; a++) {
+    
+// }
