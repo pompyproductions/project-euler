66e7bfe6d7a748125b6c3090a30a904ffb89106d
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-06 23:47:06 +0100

    Write getPythagorean function

--- 009/script.js
+++ 009/script.js
@@ -1,2 +1,17 @@
 "use strict";
 console.log("script.js: flotation device");
+
+function getPythagorean(a) {
+    let [b, c] = [a + 1, a + 2];
+    while (c*c !== a*a + b*b) {
+        while (c*c < a*a + b*b) {
+            c++;
+        }
+        if (c === b + 1) return false;
+        b++;
+    }
+
+    return [a,b,c]
+}
+
+console.log(getPythagorean(1));
