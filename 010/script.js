279b9c1b3a09323dce361202219f535af7060a97
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-07 12:30:18 +0100

    Place primes.push() inside isPrime condition

--- 010/script.js
+++ 010/script.js
@@ -2,29 +2,32 @@
 console.log("script.js: flotation device");
 
 function isFactor(factor, number) {
-    // console.log(`dividing ${number} with ${factor}`);
+    console.log(`dividing ${number} with ${factor}`);
     return !(number % factor);
 }
 
 function* primeGen(max=Infinity) {
     yield 2;
-    let primes = [2];
-    let current = 3;
-    let i = 0;
+    let primes = [2, 3];
+    let current = 5;
+    let i = 1;
 
     while (current <= max) {
         let isPrime = true;
-        while (primes[i] <= Math.trunc(current/primes[i-1])) {
+        while (primes[i] < Math.trunc(current/primes[i-1])) {
             i++;
         }
         for (let item of primes.slice(0, i+1)) {
             if (isFactor(item, current)) {
                 isPrime = false;
+                console.log("not prime");
                 break;
             }
         }
-        if (isPrime) yield current;
-        primes.push(current);
+        if (isPrime) {
+            primes.push(current);
+            yield current;
+        }
         current += 2;
     }
     console.log(primes)
@@ -33,7 +36,7 @@ function* primeGen(max=Infinity) {
 
 let sum = 0;
 console.time();
-for (let prime of primeGen(500000)) {
+for (let prime of primeGen(100)) {
     sum += prime;
 }
 console.timeEnd();
