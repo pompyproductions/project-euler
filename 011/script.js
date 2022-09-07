cbca5d495863e7471d8a8f598170cef4d8d79411
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-07 23:25:30 +0100

    Create getLine function

--- 011/script.js
+++ 011/script.js
@@ -1,6 +1,30 @@
 "use strict";
-console.time();
 console.log("script.js: flotation device");
 
+
+function getLine(arr, x, y, mode, len=4) {
+    const line = [];
+    for (let i = 0; i < len; i++) {
+        line.push(arr[y][x]);
+        if (mode === "h") x++
+        else if (mode === "v") y++
+        else if (mode === "d") {x++; y++}
+        else {return line};
+    }
+    return line
+}
+
+let largest = [];
+
 console.log(PROBLEM_INPUT);
-console.timeEnd();
+// console.log(getLine(PROBLEM_INPUT, 0, 0, "h"));
+// console.log(getLine(PROBLEM_INPUT, 0, 0, "v"));
+// console.log(getLine(PROBLEM_INPUT, 0, 0, "d"));
+// console.log(getLine(PROBLEM_INPUT, 0, 0));
+
+// console.time();
+// console.timeEnd();
+
+// for (let y; y < arr.length, y++) {
+//     for (let x)
+// }
