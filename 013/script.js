4ded39f54447ce8f041e7632c249ec0a3956d16c
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-09 00:53:33 +0100

    Test float operation idea

--- 013/script.js
+++ 013/script.js
@@ -1,5 +1,20 @@
 "use strict";
 console.log("script.js: flotation device");
 
-// console.time();
-// console.timeEnd();
+
+console.log(PROBLEM_INPUT);
+console.log(PROBLEM_INPUT[0] / Math.pow(10, 5));
+
+
+let sum;
+
+
+console.time("Integer addition");
+sum = PROBLEM_INPUT.reduce((acc, val) => acc + val, 0);
+console.log(sum)
+console.timeEnd("Integer addition");
+
+let arr = PROBLEM_INPUT.map(x => x / Math.pow(10, 5));
+console.time("Float addition");
+sum = arr.reduce((acc, val) => acc + val, 0);
+console.timeEnd("Float addition");
