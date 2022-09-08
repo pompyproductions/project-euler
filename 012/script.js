a34ab287ed6a295eb64c831a12b5426be512b1be
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-08 23:40:51 +0100

    Create triangle generator

--- 012/script.js
+++ 012/script.js
@@ -2,3 +2,17 @@
 console.log("script.js: flotation device");
 console.time();
 console.timeEnd();
+
+function* triangleGen(max=Infinity) {
+    let curr = 0;
+    for (let i=1; i<=max; i++) {
+        curr += i;
+        yield curr;
+    }
+}
+
+const generator = triangleGen(5);
+
+for (let num of generator) {
+    console.log(num);
+}
