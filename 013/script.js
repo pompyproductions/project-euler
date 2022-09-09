a3fd5457f1de69a5b5c0741c8781f07fd66db54a
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-09 01:02:46 +0100

    Solve with float arithmetic

--- 013/script.js
+++ 013/script.js
@@ -1,20 +1,11 @@
 "use strict";
 console.log("script.js: flotation device");
 
-
-console.log(PROBLEM_INPUT);
-console.log(PROBLEM_INPUT[0] / Math.pow(10, 5));
-
-
 let sum;
+let arr = PROBLEM_INPUT.map(x => x / Math.pow(10, 50));
 
-
-console.time("Integer addition");
-sum = PROBLEM_INPUT.reduce((acc, val) => acc + val, 0);
-console.log(sum)
-console.timeEnd("Integer addition");
-
-let arr = PROBLEM_INPUT.map(x => x / Math.pow(10, 5));
 console.time("Float addition");
 sum = arr.reduce((acc, val) => acc + val, 0);
 console.timeEnd("Float addition");
+
+console.log((sum * Math.pow(10, 10 - sum.toFixed(0).length)).toFixed(0))
