14c38bd46d1abbbf0665bb443d7d0d33180a39c6
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-09 22:33:27 +0100

    Change approach and extract prime divisors

--- 012/script.js
+++ 012/script.js
@@ -10,28 +10,75 @@ function* triangleGen(max=Infinity) {
     }
 }
 
-function getDivisors(num) {
-    let arr = [];
-    for (let i=1; i <= num / 2; i++) {
-        if (num % i === 0) arr.push(i);
-    }
-    arr.push(num);
-    return arr;
+// function getDivisors(num) {
+//     let arr = [1, num];
+//     for (let i=2; i <= num / 2; i++) {
+//         if (num % i === 0) {
+//             arr.push(i);
+//         }
+//     }
+//     console.log(num);
+//     console.log(arr);
+//     return arr;
+// }
+
+function isFactor(factor, number) {
+    // console.log(`dividing ${number} with ${factor}`);
+    return !(number % factor);
 }
 
-const generator = triangleGen();
-let current = generator.next();
+function* primeGen(max=Infinity) {
+    yield 2;
+    yield 3;
+    let primes = [2, 3];
+    let current = 5;
+    let i = 1;
 
-console.time();
-for (let i = 1; i <= 2000; i++) {
-    // console.log(getDivisors(current.value));
-    // console.log(getDivisors(current.value).length);
-    current = generator.next();
-    if (getDivisors(current.value).length >= 400) {
-        console.log(`Found solution in ${i} steps`);
-        break;
+    while (current <= max) {
+        let isPrime = true;
+        while (primes[i] < Math.trunc(current/primes[i-1])) {
+            i++;
+        }
+        for (let item of primes.slice(0, i+1)) {
+            if (isFactor(item, current)) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            primes.push(current);
+            yield current;
+        }
+        current += 2;
     }
+    console.log(primes)
 }
-console.log(getDivisors(current.value));
-console.log(getDivisors(current.value).length);
-console.timeEnd();
+
+
+function getPrimeDivisors(num) {
+    const divs = [];
+    const generator = primeGen();
+    let prime = generator.next();
+    while (prime && (prime.value <= num / 2)) {
+        if (isFactor(prime.value, num)) divs.push(prime.value);
+        prime = generator.next();
+    }
+    console.log(divs);
+}
+
+// const generator = triangleGen();
+// let current = generator.next();
+
+// console.time();
+// for (let i = 1; i <= 20; i++) {
+//     // console.log(getDivisors(current.value));
+//     // console.log(getDivisors(current.value).length);
+//     current = generator.next();
+//     if (getDivisors(current.value).length >= 400) {
+//         console.log(`Found solution in ${i} steps`);
+//         break;
+//     }
+// }
+// console.log(getDivisors(current.value));
+// console.log(getDivisors(current.value).length);
+// console.timeEnd();
