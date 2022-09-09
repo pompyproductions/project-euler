37026b05404793370920497541c52b09d8ec7db3
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-09 01:39:40 +0100

    Count and retain longest chain

--- 014/script.js
+++ 014/script.js
@@ -1,5 +1,28 @@
 "use strict";
 console.log("script.js: flotation device");
 
-console.time("Function 1");
-console.timeEnd("Function 1");
+// console.time("Function 1");
+// console.timeEnd("Function 1");
+
+function* collatzGen(num) {
+    // assuming all sequences end with 1
+    yield num;
+    while (num !== 1) {
+        num = num%2===0 ? num/2 : 3*num+1;
+        yield num;
+    }
+}
+
+let generator;
+let longest = [1, 1];
+
+for (let i = 2; i < 100; i++) {
+    generator = collatzGen(i);
+    let count = -1;
+    for (let step of generator) {
+        count++;
+    }
+    console.log(`${count} steps taken for ${i}`);
+    if (count > longest[1]) longest = [i, count];
+}
+console.log(longest)
