41c657cc8e78fbeb916ef05df360632abe4f0b7a
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-10 20:29:50 +0100

    Write words until 20

--- 017/script.js
+++ 017/script.js
@@ -1,8 +1,34 @@
 "use strict";
 console.log("script.js: flotation device");
 
+const WORDS = [
+    [
+        "one", "two", "three", "four", "five", "six",
+        "seven", "eight", "nine", "ten", "eleven", "twelve",
+        "thirteen", "fourteen", "fifteen", "sixteen",
+        "seventeen", "eighteen", "nineteen"
+    ],
+    [
+        "hundred", "thousand", "million"
+    ]
+]
 
-console.time("");
-console.timeEnd("");
+function getDecimalPlaces(num) {
+    num = Math.abs(num);
+    let count = 0;
+    while (num >= 1) {
+        count += 1;
+        num = Math.trunc(num / 10);
+    }
+    return count;
+}
 
-// console.timeEnd("Function 1");
+function numberToWord(num) {
+    let decimalPlaces = getDecimalPlaces(num);
+    if (num < 20) return WORDS[0][num-1];
+}
+
+
+
+console.time();
+console.timeEnd();
