1c114c37e25dae49f045d16a896da04e12555728
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-10 19:59:23 +0100

    Count the sum of the digits

--- 016/script.js
+++ 016/script.js
@@ -4,7 +4,9 @@ console.log("script.js: flotation device");
 let exp = 1;
 let val = 2;
 
-while (val < Number.MAX_SAFE_INTEGER) {
+console.time("Find 2^1000");
+
+while (val <= Number.MAX_SAFE_INTEGER / 2) {
     val *= 2;
     exp++;
 }
@@ -13,11 +15,28 @@ console.log(exp);
 val = BigInt(val);
 
 while (exp < 1000) {
-    val = val * 2n;
+    val *= 2n;
     exp++;
 }
 console.log(exp);
 console.log(val);
 
-// console.time("Function 1")
+console.timeEnd("Find 2^1000");
+console.time("Count digits");
+
+let count = 0;
+while (val > Number.MAX_SAFE_INTEGER) {
+    count += Number(val % 10n);
+    val /= 10n;
+}
+
+val = Number(val);
+while (val >= 1) {
+    count += val % 10;
+    val = Math.trunc(val / 10);
+}
+
+console.log(count);
+console.timeEnd("Count digits");
+
 // console.timeEnd("Function 1");
