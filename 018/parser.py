2277ae738397590b45607578b4e26f69fbd7fc08
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-10 23:59:01 +0100

    Parse problem input

--- 018/parser.py
+++ 018/parser.py
@@ -3,21 +3,24 @@ import re
 f1 = open("input-raw.html")
 f2 = open("input-parsed.js", "w")
 
-# f2.write("const PROBLEM_INPUT = [\n")
+f2.write("const PROBLEM_INPUT = [\n")
 
 inputArr = []
 
 for line in f1:
-    # strip unnecessary stuff
-    # line = re.sub(r"<\/?.+?>", "", line) # made non-greedy with "+?"
-    # line = re.sub(r"\n", "", line)
     
-    num = re.match(r"^(\d.)", line)
-    if num:
-        print(num)
-        # f2.write("    " + num.groups()[0] + ",\n")
+    numbersMatch = re.search(r"^(\d\d( \d\d)*)", line)
+    if numbersMatch:
+        f2.write("    [")
+        arr = numbersMatch.groups()[0].split(" ")
+        f2.write(arr.pop(0))
+        for str in arr:
+            f2.write(", " + str)
+        f2.write("],\n")
+
+    
 
 f1.close()
 
-# f2.write("]")
-# f2.close()
+f2.write("]")
+f2.close()
