b6b33efb7d3a37668347b128d4d2bd740ad2add7
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-11 00:19:42 +0100

    Get maximum path

--- 018/script.js
+++ 018/script.js
@@ -1,7 +1,20 @@
 "use strict";
 console.log("script.js: flotation device");
 
-console.log(PROBLEM_INPUT)
+console.log(PROBLEM_INPUT);
+
+const path = [PROBLEM_INPUT[0][0]];
+let lastIndex = 0;
+for (let i=1; i<PROBLEM_INPUT.length; i++) {
+    let choice = PROBLEM_INPUT[i][lastIndex];
+    if (PROBLEM_INPUT[i][lastIndex+1] > choice) {
+        choice = PROBLEM_INPUT[i][lastIndex+1];
+        lastIndex++;
+    }
+    path.push(choice);
+}
+console.log(path);
+
 
 console.time();
 console.timeEnd();
