7b93026fd7fb8ca19a1555461126a7990a5a9fb1
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-10 19:42:07 +0100

    Convert to BigInt after reaching max

--- 016/script.js
+++ 016/script.js
@@ -1,5 +1,23 @@
 "use strict";
 console.log("script.js: flotation device");
 
+let exp = 1;
+let val = 2;
+
+while (val < Number.MAX_SAFE_INTEGER) {
+    val *= 2;
+    exp++;
+}
+console.log(exp);
+
+val = BigInt(val);
+
+while (exp < 1000) {
+    val = val * 2n;
+    exp++;
+}
+console.log(exp);
+console.log(val);
+
 // console.time("Function 1")
 // console.timeEnd("Function 1");
