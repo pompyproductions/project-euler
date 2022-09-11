3a3e26b5cb563d7360d3cce63df1abf563503893
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-11 18:49:30 +0100

    Create factorial generator

--- 020/script.js
+++ 020/script.js
@@ -1,5 +1,19 @@
 "use strict";
 console.log("script.js: flotation device");
 
+function* factorialGen(num=Infinity) {
+    let prev = 1;
+    for (let i=1; i < num; i++) {
+        prev *= i;
+        yield prev; 
+    }
+}
+
+const generator = factorialGen(10);
+for (let factor of generator) {
+    console.log(factor);
+}
+
+
 // console.time("Function 1");
 // console.timeEnd("Function 1");
