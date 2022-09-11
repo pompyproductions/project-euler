a1680d18c9ce4fe2a369eca09cb18e1078a09598
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-11 14:23:06 +0100

    Create Pyramid class

--- 018/script.js
+++ 018/script.js
@@ -4,27 +4,73 @@ console.log("script.js: flotation device");
 console.log(PROBLEM_INPUT);
 
 class Pyramid {
+    floors;
+    depth;
+
+    current;
+    maxDescent;
+    greedyDescent;
+
     constructor(input) {
         this.floors = input;
-        // this.currentFloor = this.floors.pop();
+        this.depth = input.length;
+        
+        this.maxDescent = [];
+        this.greedyDescent = []
+        for (let floor of this.floors) {
+            this.maxDescent.push(floor.reduce((acc,val)=> val > acc ? val : acc, 0));
+            this.greedyDescent.push()
+        }
+        let index = 0;
+        for (let i = 0; i < this.depth; i++) {
+            let best = this.floors[i][index];
+            if (this.floors[i][index + 1] > best) {
+                best = this.floors[i][index++ + 1];
+            }
+            this.greedyDescent.push(best);
+        }
+        
+        
+        this.current = [0, [[0, this.floors[1][0]]]]; // current floor and an array of paths
     }
+
+    descend([ind, sum]) {
+        if (
+            sum + this.maxDescent.slice(this.current[0]+1, this.depth).reduce((acc,val)=>acc+val,0) >
+            this.greedyDescent.reduce((acc,val)=>acc+val, 0)
+        ) {
+            return [
+                [ind, sum + this.floors[this.current[0]+1][ind]],
+                [ind + 1, sum + this.floors[this.current[0]+1][ind + 1]]
+            ]
+        } return false;
+    }
+
+    explore() {
+        const next = [this.current[0] + 1, []]
+        for (let path of this.current[1]) {
+            let arr = this.descend(path);
+            if (arr) {next[1].push(...arr)};
+        }
+        this.current = next;
+        return this.current;
+    }
+
+    // lookAhead(index) {
+    //     // greedy descent: picks the higher value at every step
+    //     // check if current path
+    //     let sum = 0;
+    //     for (let i = this.current[0]; i < this.depth; i++) {
+    //         let best = this.floors[i][index];
+    //         if (this.floors[i][index + 1] > best) {
+    //             best = this.floors[i][index++ + 1];
+    //         }
+    //         sum += best;
+    //     }
+    //     console.log(sum);
+    // }
 }
 
-
-
-// const path = [PROBLEM_INPUT[0][0]];
-// let lastIndex = 0;
-
-// // find biggest possible path:
-// for (let i=1; i<PROBLEM_INPUT.length; i++) {
-//     let choice = PROBLEM_INPUT[i][lastIndex];
-//     if (PROBLEM_INPUT[i][lastIndex+1] > choice) {
-//         choice = PROBLEM_INPUT[i][lastIndex+1];
-//         lastIndex++;
-//     }
-//     path.push(choice);
-// }
-// console.log(path.reduce((acc, val) => acc + val, 0));
 
 
 console.time();
