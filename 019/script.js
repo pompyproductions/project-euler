da6398251b5226f7585ecd65681fce017353f92f
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-11 15:19:39 +0100

    Write isLeapYear function

--- 019/script.js
+++ 019/script.js
@@ -1,5 +1,38 @@
 "use strict";
 console.log("script.js: flotation device");
 
+const DAYS = [
+    "mon", "tue", "wed", "thu",
+    "fri", "sat", "sun"
+]
+const MONTHS = [
+    31, 28, 31, 30, 31, 30, 
+    31, 31, 30, 31, 30, 31
+]
+
+function isLeapYear(year) {
+    if (!(year % 4)) {
+
+        if (!(year % 100)) {
+            if (!(year % 400)) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
+}
+
+// function skipMonth(year, prevMonth) {
+//     if (prevMonth === 2 && ) 
+//     if 
+//     return [year, prevMonth + 1];
+// }
+
+let currentDate = [1900, 1];
+// currentDate = skipMonth(...currentDate);
+
+
 console.time();
 console.timeEnd();
