4261ce21d4a52c7deb9a695a97729784e245481f
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-12 22:19:05 +0100

    Loop through generator

--- 025/script.js
+++ 025/script.js
@@ -2,9 +2,9 @@
 console.log("script.js: flotation device");
 
 function* fibonacciGen(max=Infinity) {
-    yield 1;
-    yield 1;
-    const allFibos = [1, 1];
+    yield 1n;
+    yield 1n;
+    const allFibos = [1n, 1n];
 
     for (let i = 0; i < max; i++) {
         let fibo = allFibos[i] + allFibos[i + 1];
@@ -14,7 +14,7 @@ function* fibonacciGen(max=Infinity) {
 }
 
 function getDigitCount(num) {
-    num = BigInt(Math.trunc(Math.abs(num)));
+    num = BigInt(num);
     let i = 0;
     while (num >= 1n) {
         i++;
@@ -25,14 +25,14 @@ function getDigitCount(num) {
 
 let generator = fibonacciGen();
 let i = 0;
-for (; i < 1400; i++) {
+for (; i < 5000; i++) {
     let fibo = generator.next().value;
     // console.log(`${fibo}: ${getDigitCount(fibo)} digits`);
     if (getDigitCount(fibo) === 1000) {
-        console.log(`found: ${fibo}`);
+        console.log(`found: ${fibo}, index ${i+1}`);
+        break;
     }
 }
-console.log(i);
 
 // console.time("Function 1");
 // console.timeEnd("Function 1");
