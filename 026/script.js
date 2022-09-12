18e8c808f3df0861fa01b190dec3cef516d28b6b
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-12 23:07:53 +0100

    Loop over recurring cycle count

--- 026/script.js
+++ 026/script.js
@@ -1,21 +1,31 @@
 "use strict";
 console.log("script.js: flotation device");
 
-function getRecurringCycles(num) {
+function countRecurringCycles(num) {
     let r = 1;
     let remainders = [];
     while (r !== 0) {
         while (r <= num) {
             r *= 10;
         }
-        r = Math.trunc(r);
         r %= num;
-        console.log(r);
         if (remainders.some(a => a === r)) return remainders.length;
         remainders.push(r);
     }
     return 0;
 
 }
-// console.time("Function 1");
-// console.timeEnd("Function 1");
+
+let longest = [0, 0];
+console.time("Function 1");
+
+for (let d = 2; d < 1000; d++) {
+    let count = countRecurringCycles(d);
+    if (count > longest[1]) {
+        longest = [d, count]
+    };
+}
+
+console.timeEnd("Function 1");
+console.log(longest);
+
