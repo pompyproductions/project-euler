659248efc99aaa7449f261e12fbc52972f743f72
Eren Gazioglu <pompyproduction@gmail.com>
2022-09-12 21:56:43 +0100

    Create fibonacci generator

--- 025/script.js
+++ 025/script.js
@@ -1,5 +1,21 @@
 "use strict";
 console.log("script.js: flotation device");
 
+function* fibonacciGen(max=Infinity) {
+    yield 1;
+    yield 1;
+    const allFibos = [1, 1];
+
+    for (let i = 0; i < max; i++) {
+        let fibo = allFibos[i] + allFibos[i + 1];
+        allFibos.push(fibo);
+        yield fibo;
+    }
+}
+
+for (let fibo of fibonacciGen(20)) {
+    console.log(fibo);
+}
+
 // console.time("Function 1");
 // console.timeEnd("Function 1");
